37165b41b1e9475f5dc2df5aa8d3432458c2b03a
Bruno Silva <bruno3dcontato@gmail.com>
2024-12-30 17:46:31 -0300

    feat: improve zod generator

--- lib/generators/zod.ts
+++ lib/generators/zod.ts
@@ -1,23 +1,34 @@
-export function generateZodSchema(model: any) {
+import { Field, Model, PrismaScalarType } from '@/lib/types/model';
+
+export function generateZodSchema(model: Model) {
   return `
 import { z } from "zod";
 
 export const ${model.name}Schema = z.object({
-  ${model.fields
-    .map((field: any) => `${field.name}: ${mapToZodType(field.type)}`)
-    .join(",\n  ")}
+  ${model.fields.map((field) => `${field.name}: ${mapToZodField(field)}`).join(',\n  ')}
 });
 
 export type ${model.name} = z.infer<typeof ${model.name}Schema>;
 `;
 }
 
-function mapToZodType(type: string): string {
-  const typeMap: Record<string, string> = {
-    string: "z.string()",
-    number: "z.number()",
-    boolean: "z.boolean()",
-    date: "z.date()",
+function mapToZodField(field: Field): string {
+  const typeMap: Record<PrismaScalarType, string> = {
+    String: 'z.string()',
+    Int: 'z.number().int()',
+    Float: 'z.number()',
+    Boolean: 'z.boolean()',
+    DateTime: 'z.date()',
+    Json: 'z.any()',
+    BigInt: 'z.bigint()',
+    Decimal: 'z.number()',
   };
-  return typeMap[type] || "z.string()";
+
+  let zodField = typeMap[field.type as PrismaScalarType] || 'z.string()';
+
+  if (!field.required) {
+    zodField += '.optional()';
+  }
+
+  return zodField;
 }
